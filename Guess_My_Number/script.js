baae088af266926099b8862955dc22665802f020
YajneshVishwakarma <75421738+YajneshVishwakarma@users.noreply.github.com>
2022-06-24 22:08:54 +0530

    Update script.js

--- Guess_My_Number/script.js
+++ Guess_My_Number/script.js
@@ -1,14 +1,17 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
+// console.log(document.querySelector('.message').textContent);
 
-document.querySelector('.message').textContent = '🥳 Correct Answer!';
-console.log(document.querySelector('.guess').value);
+// document.querySelector('.message').textContent = '🥳 Correct Answer!';
+// console.log(document.querySelector('.guess').value);
 
-const changeScore = function(){
-    const val = document.querySelector('.guess').value;
-    document.querySelector('.score').textContent = val;
-}
+const changeScore = function () {
+    const val = document.querySelector('.guess');
+    let condition = Number(val.value);
+    const msg = document.querySelector('.message');
+    if (!condition) {
+        msg.textContent = '🚫 No number provided!!'
+    }
+};
 
-document.querySelector('.check').addEventListener('click',changeScore);
-    
+document.querySelector('.check').addEventListener('click', changeScore);
