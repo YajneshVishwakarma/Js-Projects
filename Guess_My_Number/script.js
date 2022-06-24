2e883aa831ff7c68e773cb8ec49ee855d207e8c3
YajneshVishwakarma <75421738+YajneshVishwakarma@users.noreply.github.com>
2022-06-24 21:54:41 +0530

    Update script.js

--- Guess_My_Number/script.js
+++ Guess_My_Number/script.js
@@ -1,3 +1,14 @@
 'use strict';
 
 console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = '🥳 Correct Answer!';
+console.log(document.querySelector('.guess').value);
+
+const changeScore = function(){
+    const val = document.querySelector('.guess').value;
+    document.querySelector('.score').textContent = val;
+}
+
+document.querySelector('.check').addEventListener('click',changeScore);
+    
