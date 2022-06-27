6a7cb17a26964fd0f3fe743c5f3c79e0d5c352f1
YajneshVishwakarma <75421738+YajneshVishwakarma@users.noreply.github.com>
2022-06-27 19:41:54 +0530

    Update script.js

--- Guess_My_Number/script.js
+++ Guess_My_Number/script.js
@@ -5,13 +5,30 @@
 // document.querySelector('.message').textContent = '🥳 Correct Answer!';
 // console.log(document.querySelector('.guess').value);
 
-const changeScore = function () {
-    const val = document.querySelector('.guess');
-    let condition = Number(val.value);
-    const msg = document.querySelector('.message');
-    if (!condition) {
-        msg.textContent = '🚫 No number provided!!'
-    }
-};
+const n = Math.trunc(Math.random() * 20) + 1;
+console.log(n);
 
-document.querySelector('.check').addEventListener('click', changeScore);
+document.querySelector('.check').addEventListener('click', function () {
+  const val = document.querySelector('.guess');
+  let condition = Number(val.value);
+
+  const msg = document.querySelector('.message');
+  const score = document.querySelector('.score');
+  let sc = 20;
+
+  if (!condition) {
+    msg.textContent = '🚫 No number provided!';
+    console.log(typeof score.value);
+  } else if (condition > n) {
+    msg.textContent = 'Too High!';
+    sc--;
+    document.querySelector('.score').textContent = sc;
+    console.log(score.value);
+  } else if (condition < n) {
+    msg.textContent = 'Too low!';
+    sc--;
+    document.querySelector('.score').textContent = sc;
+  } else if (condition === n) {
+    msg.textContent = '🥳 Correct Answer!';
+  }
+};);
