df0412dbff305e306f368791b3c2d67d2edfdf1c
YajneshVishwakarma <75421738+YajneshVishwakarma@users.noreply.github.com>
2022-06-30 21:57:32 +0530

    Update script.js

--- Guess_My_Number/script.js
+++ Guess_My_Number/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
+// console.log(document.querySelector('.message').textContent);
 
 // document.querySelector('.message').textContent = '🥳 Correct Answer!';
 // console.log(document.querySelector('.guess').value);
@@ -8,27 +8,30 @@ console.log(document.querySelector('.message').textContent);
 const n = Math.trunc(Math.random() * 20) + 1;
 console.log(n);
 
+const score = document.querySelector('.score');
+const msg = document.querySelector('.message');
+
+let sc = score.textContent;
+
 document.querySelector('.check').addEventListener('click', function () {
   const val = document.querySelector('.guess');
   let condition = Number(val.value);
-
-  const msg = document.querySelector('.message');
-  const score = document.querySelector('.score');
-  let sc = 20;
-
+  
   if (!condition) {
     msg.textContent = '🚫 No number provided!';
-    console.log(typeof score.value);
+    console.log(typeof score.textContent);
   } else if (condition > n) {
     msg.textContent = 'Too High!';
     sc--;
     document.querySelector('.score').textContent = sc;
-    console.log(score.value);
+    console.log(score.textContent);
   } else if (condition < n) {
     msg.textContent = 'Too low!';
     sc--;
     document.querySelector('.score').textContent = sc;
+    console.log(score.textContent);
   } else if (condition === n) {
     msg.textContent = '🥳 Correct Answer!';
+    console.log(score.textContent);
   }
-};);
+});
