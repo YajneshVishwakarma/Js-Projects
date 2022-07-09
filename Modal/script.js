f9d132e39a721b1489ca48edfc38a92c3fc2f7c6
YajneshVishwakarma <75421738+YajneshVishwakarma@users.noreply.github.com>
2022-07-09 10:01:27 +0530

    Update script.js

--- Modal/script.js
+++ Modal/script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const showModalBtn = document.querySelectorAll('.show-modal');
+const closeModalBtn = document.querySelector('.close-modal');
+const overlay = document.querySelector('.overlay');
+const hidden = document.querySelector('.hidden');
+
+const openModel = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < showModalBtn.length; i++) {
+  showModalBtn[i].addEventListener('click', openModel);
+}
+
+closeModalBtn.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
